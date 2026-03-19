360ffec347028a8a7a5a64880f9b5f76d84245aa
liamcarey <94330852+liamcarey@users.noreply.github.com>
2026-03-19 16:40:59 -0400

    Create SonarTest.java

---
+++ SonarTest.java
@@ -0,0 +1,13 @@
+public class SonarExample {
+    public static void main(String[] args) {
+        String text = null;
+
+        if (text.equals("hello")) {   // Bug: possible NullPointerException
+            System.out.println("Hi");
+        }
+
+        int unused = 5;               // Code smell: unused local variable
+
+        System.out.println("Done");
+    }
+}
